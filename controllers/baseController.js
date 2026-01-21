777aa4ce77ece36a75eca700068a6adea0f28528
Jared Samayoa <jsamayoapenagos@byupathway.edu>
2026-01-21 17:40:15 -0600

    W03 Learning Activities: Content Delivery and the MVC Pattern

---
+++ controllers/baseController.js
@@ -0,0 +1,9 @@
+const utilities = require("../utilities/")
+const baseController = {}
+
+baseController.buildHome = async function(req, res){ // <-- Method 1 to export the function (implicit): "baseController.buildHome"
+  // const nav = await utilities.getNav()
+  res.render("index", {title: "Home", nav})
+}
+
+module.exports = baseController // <-- Method 2 to export the function
